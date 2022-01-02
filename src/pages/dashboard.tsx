8912c4f8cd7bfca0db6f29e6bc1985bea94a2066
victor de souza magaldi <victmagaldi@hotmail.com>
2022-01-02 19:55:31 -0300

    insert graphic chart

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, SimpleGrid, Text } from "@chakra-ui/react";
+import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
 import dynamic from "next/dynamic";
 import { Header } from "../components/Header/Header";
 import { Sidebar } from "../components/Sidebar/Sidebar";
@@ -13,7 +13,53 @@ const ReactApexCharts = dynamic(
     }
 );
 
-const options = {};
+const options: ApexCharts.ApexOptions = {
+    chart: {
+        toolbar: {
+            show: false,
+        },
+        zoom: {
+            enabled: false,
+        },
+        foreColor: theme?.colors.gray[500],
+    },
+    grid: {
+        show: false,
+    },
+    dataLabels: {
+        enabled: false,
+    },
+    tooltip: {
+        enabled: false,
+    },
+    xaxis: {
+        type: "datetime",
+        axisBorder: {
+            color: theme.colors.gray[600],
+        },
+        axisTicks: {
+            color: theme.colors.gray[600],
+        },
+        categories: [
+            "2021-03-17T00:00:00.000Z",
+            "2021-03-18T00:00:00.000Z",
+            "2021-03-19T00:00:00.000Z",
+            "2021-03-20T00:00:00.000Z",
+            "2021-03-21T00:00:00.000Z",
+            "2021-03-22T00:00:00.000Z",
+            "2021-03-23T00:00:00.000Z",
+        ],
+    },
+    fill: {
+        opacity: 0.3,
+        type: "gradient",
+        gradient: {
+            shade: "dark",
+            opacityFrom: 0.7,
+            opacityTo: 0.3,
+        },
+    },
+};
 const series = [{ name: "series 01", data: [3, 101, 25, 30, 28, 15, 109] }];
 
 export default function Dashboard() {
@@ -29,7 +75,7 @@ export default function Dashboard() {
                     minChildWidth="320px"
                     align="flex-start"
                 >
-                    <Box p="8" bgColor="gray.800" borderRadius={8}>
+                    <Box p="8" bgColor="gray.800" borderRadius={8} pb="4">
                         <Text fontSize="lg" mb="4">
                             Inscritos da Semana
                         </Text>
@@ -40,10 +86,16 @@ export default function Dashboard() {
                             series={series}
                         />
                     </Box>
-                    <Box p="8" bgColor="gray.800" borderRadius={8}>
+                    <Box p="8" bgColor="gray.800" borderRadius={8} pb="4">
                         <Text fontSize="lg" mb="4">
                             Taxa de Abertura
                         </Text>
+                        <ReactApexCharts
+                            type="area"
+                            height="160"
+                            options={options}
+                            series={series}
+                        />
                     </Box>
                 </SimpleGrid>
             </Flex>
