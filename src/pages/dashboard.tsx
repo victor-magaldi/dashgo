5c7b623c2a34324cd3acb26ed3aad698e6491dbd
victor de souza magaldi <victmagaldi@hotmail.com>
2022-01-02 19:20:13 -0300

    Init box in dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,4 +1,4 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex, SimpleGrid, Text } from "@chakra-ui/react";
 import { Header } from "../components/Header/Header";
 import { Sidebar } from "../components/Sidebar/Sidebar";
 
@@ -8,6 +8,24 @@ export default function Dashboard() {
             <Header />
             <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
                 <Sidebar />
+
+                <SimpleGrid
+                    flex="1"
+                    gap="4"
+                    minChildWidth="320px"
+                    align="flex-start"
+                >
+                    <Box p="8" bgColor="gray.800" borderRadius={8}>
+                        <Text fontSize="lg" mb="4">
+                            Inscritos da Semana
+                        </Text>
+                    </Box>
+                    <Box p="8" bgColor="gray.800" borderRadius={8}>
+                        <Text fontSize="lg" mb="4">
+                            Taxa de Abertura
+                        </Text>
+                    </Box>
+                </SimpleGrid>
             </Flex>
         </Flex>
     );
