1cf5410a4702f3690c3b572c83f1f4347654b9c1
victor de souza magaldi <victmagaldi@hotmail.com>
2022-01-02 19:38:47 -0300

    Removendo SSR da lib que necessita do window

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,20 @@
 import { Box, Flex, SimpleGrid, Text } from "@chakra-ui/react";
+import dynamic from "next/dynamic";
 import { Header } from "../components/Header/Header";
 import { Sidebar } from "../components/Sidebar/Sidebar";
+
+// Removendo SSR da lib que necessita do window (client)
+const ReactApexCharts = dynamic(
+    () => {
+        return import("react-apexcharts");
+    },
+    {
+        ssr: false,
+    }
+);
+
+const options = {};
+const series = [{ name: "series 01", data: [3, 101, 25, 30, 28, 15, 109] }];
 
 export default function Dashboard() {
     return (
@@ -19,6 +33,12 @@ export default function Dashboard() {
                         <Text fontSize="lg" mb="4">
                             Inscritos da Semana
                         </Text>
+                        <ReactApexCharts
+                            type="area"
+                            height="160"
+                            options={options}
+                            series={series}
+                        />
                     </Box>
                     <Box p="8" bgColor="gray.800" borderRadius={8}>
                         <Text fontSize="lg" mb="4">
