61b4d942c2074a38e8c7d3f934f83870548ba2ba
victor de souza magaldi <victmagaldi@hotmail.com>
2022-01-13 21:55:36 -0300

    next link init

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,23 +1,27 @@
 import {
     Icon,
-    Link,
+    Link as Chakralink,
     Text,
     LinkProps as ChakraLinkProps,
 } from "@chakra-ui/react";
 import React, { ElementType } from "react";
+import Link from "next/link";
 
 interface NavLinkProps extends ChakraLinkProps {
     icon: ElementType;
     children: string;
+    href: string;
 }
 
-export function NavLink({ icon, children, ...rest }: NavLinkProps) {
+export function NavLink({ icon, children, href, ...rest }: NavLinkProps) {
     return (
-        <Link display="flex" align="center" {...rest}>
-            <Icon as={icon} fontSize="20" />
-            <Text ml="4" fontWeight="medium">
-                {children}
-            </Text>
+        <Link href={href} passHref>
+            <Chakralink display="flex" align="center" {...rest}>
+                <Icon as={icon} fontSize="20" />
+                <Text ml="4" fontWeight="medium">
+                    {children}
+                </Text>
+            </Chakralink>
         </Link>
     );
 }
