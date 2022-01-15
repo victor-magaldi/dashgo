decc27187fe5f913156b2e1d188f14d972bd6b3a
victor de souza magaldi <victmagaldi@hotmail.com>
2022-01-14 21:49:02 -0300

    Create Drawer chakra

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,8 +1,37 @@
-import { Box } from "@chakra-ui/react";
+import {
+    Box,
+    Drawer,
+    DrawerBody,
+    DrawerCloseButton,
+    DrawerContent,
+    DrawerHeader,
+    DrawerOverlay,
+    useBreakpointValue,
+} from "@chakra-ui/react";
 
 import { SidebarNav } from "./SidebarNav";
 
 export function Sidebar() {
+    const isDrawerSidebar = useBreakpointValue({
+        base: true,
+        lg: false,
+    });
+
+    if (isDrawerSidebar) {
+        return (
+            <Drawer isOpen={true} placement="left" onClose={() => {}}>
+                <DrawerOverlay>
+                    <DrawerContent bg={"gray.800"} p="4">
+                        <DrawerCloseButton mt="6" />
+                        <DrawerHeader>Navegação</DrawerHeader>
+                        <DrawerBody>
+                            <SidebarNav />
+                        </DrawerBody>
+                    </DrawerContent>
+                </DrawerOverlay>
+            </Drawer>
+        );
+    }
     return (
         <Box as="aside" w="64" mr="8">
             <SidebarNav />
