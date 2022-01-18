f9389e3ec4daaf9155554508ea17696159e484fa
victor de souza magaldi <victmagaldi@hotmail.com>
2022-01-18 20:31:34 -0300

    encaminhamento de refs

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -4,13 +4,16 @@ import {
     Input as ChakraInput,
     InputProps as ChakraInputProps,
 } from "@chakra-ui/react";
-import { forwardRef } from "react";
+import { forwardRef, ForwardRefRenderFunction } from "react";
 
 interface InputProps extends ChakraInputProps {
     name: string;
     label?: string;
 }
-const InputBase = ({ name, label, ...rest }: InputProps) => {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
+    { name, label, ...rest },
+    ref
+) => {
     return (
         <FormControl>
             {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
@@ -26,6 +29,7 @@ const InputBase = ({ name, label, ...rest }: InputProps) => {
                     bgColor: "gray.900",
                 }}
                 size="lg"
+                ref={ref}
                 {...rest}
             />
         </FormControl>
